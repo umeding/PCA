83bb4e8d97cdc5a30e54c9bc18108e1f991d5e3b
Uwe Meding <uwe@uwemeding.com>
2013-08-26 04:22:03 -0400

    updates

--- src/main/java/com/uwemeding/pca/PCA.java
+++ src/main/java/com/uwemeding/pca/PCA.java
@@ -1,5 +1,8 @@
 package com.uwemeding.pca;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 /**
  *
  * @author uwe
@@ -22,32 +25,48 @@ public class PCA {
 		return result;
 	}
 
-	public void execute() {
+	private Matrix diag(double[] v) {
+		return null;
+	}
 
-		double[] data = Data.small_vector;
+	public void execute() throws Exception {
+
+		double[] data = Data.ts;
 		System.out.println("data len=" + data.length);
-
-		double mean = calcMean(data);
-		System.out.println("mean: " + mean);
-
-		double[] v = centerVector(data, mean);
 
 
 		Matrix m = new ToeplitzMatrix(data, ToeplitzMatrix.Type.Symmetrical);
-		
+
 //		m = m.transpose();
-		log("Toeplitz matrix", m);
+//		log("Toeplitz matrix", m);
 //		Matrix m = new TrajectoryMatrix(data, 4);
 //		log("Trajectory matrix", m);
 
+		System.out.println("Centering matrix");
 		m = m.center();
-		log("Centered", m);
-		
-		SingularValueDecomposition svd = new SingularValueDecomposition(m);
-	log("SVD S", svd.getS());	
-	log("SVD U", svd.getU());	
-	log("SVD V", svd.getV());	
+//		log("Centered", m);
 
+		System.out.println("Running SVD");
+		SingularValueDecomposition svd = new SingularValueDecomposition(m);
+//		log("SVD S", svd.getS());
+//		log("SVD U", svd.getU());
+//		log("SVD V", svd.getV());
+
+//		double norm = 2. / (m.getNRows() - 1);
+//		Matrix lambda  = svd.getS();
+
+
+		System.out.println("Calculating principle components");
+		Matrix comprinc = m.times(svd.getV());
+//		log("comprinc", comprinc);
+
+
+		System.out.println("Dumping principle components");
+		try (PrintWriter fp = new PrintWriter(new FileWriter("pcall"));) {
+			comprinc.print(fp, 1, 2);
+		}
+		System.out.println("done");
+		System.exit(0);
 
 		Matrix cov = KL.covariance(m.transpose());
 		log("Covariance", cov);
@@ -65,20 +84,22 @@ public class PCA {
 		Matrix pc = eigenVectors.transpose().times(m);
 		log("Principal component", pc);
 	}
-private static void dumpContributingFactors(Matrix org, Matrix matrix, int nfactors) {
+
+	private static void dumpContributingFactors(Matrix org, Matrix matrix, int nfactors) {
 		System.out.println("Contributing factors");
 		double[][] m = matrix.getArray();
 		double[][] o = org.getArray();
 		int ncols = matrix.getColumnDimension();
-		for(int i=0;i<matrix.getRowDimension();i++) {
-			System.out.print(i+" "+o[i][0]+" ");
-			for(int j = 0; j < nfactors; j++) {
-			    System.out.print(m[i][ncols-1-j]+" ");
+		for (int i = 0; i < matrix.getRowDimension(); i++) {
+			System.out.print(i + " " + o[i][0] + " ");
+			for (int j = 0; j < nfactors; j++) {
+				System.out.print(m[i][ncols - 1 - j] + " ");
 			}
 			System.out.println();
 		}
 	}
 // Log some stuff
+
 	private static void log(String name, Matrix m) {
 		System.out.println(name + ":");
 		m.print(1, 2);
